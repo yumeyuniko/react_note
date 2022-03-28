5a3c446604678d3eb2e2b388e00c4aad3c4d36b4
yumeyuniko <daigocafe@msn.com>
2022-03-28 23:21:26 +0900

    delete Note

--- src/App.js
+++ src/App.js
@@ -1,5 +1,56 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import NotesList from './components/NotesList';
+
 const App = () => {
-  return <p>hello rea¥ct</p>;
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: 'This is my first note',
+      date: '15/04/2022',
+    },
+    {
+      id: nanoid(),
+      text: 'This is my second note1',
+      date: '15/04/2022',
+    },
+    {
+      id: nanoid(),
+      text: 'This is my third note2',
+      date: '15/04/2022',
+    },
+    {
+      id: nanoid(),
+      text: 'This is my new note',
+      date: '31/04/2022',
+    },
+  ]);
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
+  return (
+    <div className="container">
+      <NotesList
+        notes={notes}
+        handleAddNote={addNote}
+        handleDeleteNote={deleteNote}
+      />
+    </div>
+  );
 };
 
 export default App;
